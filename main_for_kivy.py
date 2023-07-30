214d1971b90b30c4f1401e84feb9f2e35c6aa940
Ilya <fingerstylena@gmail.com>
2023-07-30 12:53:21 +0300

    add

--- main_for_kivy.py
+++ main_for_kivy.py
@@ -10,29 +10,34 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from main import func_temperature_today
 
-Window.title = "title"
 Window.size = (1280, 520)
 Window.clearcolor = (255, 255, 255, 1)
 
 
 class My_prognozApp(App):
 
-    # переменные
+    # переменные и добавление всего в окно
     def __init__(self):
         super().__init__()
+
+
         self.layout_temp = GridLayout(cols=3)
         self.layout_but_inp = GridLayout(cols=3, row_force_default=True, row_default_height=100, size_hint_y=0.3)
         self.lay_button = BoxLayout(orientation='vertical')
 
         self.button_enter = Button(text="Нажми на меня для подтверждения действий)")
         self.button_for_list = Button(text="Нажми для смены дня)")
+        self.city_input = TextInput(multiline=False, hint_text="Введите город")
+
+
         self.button_for_list.bind(on_press=self.drop_down_list_button)
         self.button_enter.bind(on_press=self.for_text)
-        self.city_input = TextInput(multiline=False, hint_text="Введите город")
+
 
         self.values_list = list()
         self.city = ""
         self.day = 'сегодня'
+
 
         self.lay_button.add_widget(self.button_for_list)
         self.layout_but_inp.add_widget(self.city_input)
@@ -51,19 +56,21 @@ class My_prognozApp(App):
         self.lay_button.add_widget(b_today)
         self.lay_button.add_widget(b_tomorrow)
 
+    # функция для кнопки сегодня
     def change_of_today(self, *args):
         self.lay_button.clear_widgets()
         self.day = 'сегодня'
         self.lay_button.add_widget(self.button_for_list)
         self.button_for_list.bind(on_press=self.drop_down_list_button)
 
+    # функция для кнопки завтра
     def change_of_tomorrow(self, *args):
         self.lay_button.clear_widgets()
         self.day = 'завтра'
         self.lay_button.add_widget(self.button_for_list)
         self.button_for_list.bind(on_press=self.drop_down_list_button)
 
-    # функция для кнопки
+    # функция по выводу температуры и т.д
     def for_text(self, *args):
         self.layout_temp.clear_widgets()
         self.values_list = func_temperature_today(city=self.city_input.text, day=self.day)
@@ -80,7 +87,7 @@ class My_prognozApp(App):
             self.layout_temp.add_widget(Label(text='Введи реальный город', font_size=50, color=(0, 0, 0, 1)))
         self.day = 'сегодня'
 
-    # Чарли и фабрика по созданию виджетов
+    # Чарли и фабрика по созданию виджетов, а точнее создание всего и вся
     def build(self) -> BoxLayout:
         box = BoxLayout(orientation='vertical')
         box.size_hint_y = 1
